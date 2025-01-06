ab9dc43b40673946470420abd1ba1af0bad116e3
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-01-06 13:32:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.side = sides
+    }
+
+    get countSides() {
+        return this.side.length
+    }
+
+    get perimeter() {
+        return this.side.reduce((a,b) => a+b, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        return this.countSides === 3 && this.side.every(s => s === this.side[0])
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.countSides === 4 && this.side.every(s => s === this.side[0])
+    }
+
+    get area() {
+        return this.side[0] ** 2
+    }
+}
+
+let shape2 = new Square([2,2,7,2])
+console.log(shape2.isValid)
